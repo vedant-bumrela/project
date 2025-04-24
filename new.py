cf65163e4b486b83ba45e8eab48e56edeb9bcf01
Vedant <vedantbumrela1125@gmail.com>
2025-04-24 09:27:16 +0530

    Updated py file

--- new.py
+++ new.py
@@ -1,10 +1,24 @@
-first = "Vedant"
-last = "Kumar"
+items = [
+    ("item1", 10),
+    ("item2", 9),
+    ("item3", 11),
+    ("item4", 7),
+]
 
-age = 20
+# def sort_items(items):
+#     return items[1]
+    
 
-print(f"Hello {first} {last}, you are {age} years old.")
-print("Hello " + first + " " + last + ", you are " + str(age) + " years old.")
-print("Hello {} {}, you are {} years old.".format(first, last, age))
-print("Hello %s %s, you are %d years old." % (first, last, age))
-print("Hello {0} {1}, you are {2} years old.".format(first, last, age))
+# items.sort(key=sort_items,reverse=True)
+# print(items)
+
+# sortd_list = sorted(items, key=lambda x: x[1])
+# print(sortd_list)
+
+x = map(lambda x: x[1], items)
+print(list(x))
+print([item[1] for item in items])
+
+y = filter(lambda x: x[1] > 9,items)
+print(list(y))
+print([item for item in items if item[1] > 9])
